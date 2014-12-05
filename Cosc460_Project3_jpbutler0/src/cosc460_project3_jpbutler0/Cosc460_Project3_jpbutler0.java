9573039e7661e5b9ce571a4cb5abc7ac6f834f24
jackal390iv <jonpbiv@gmail.com>
2014-12-05 15:08:34 -0500

    created project

---
+++ Cosc460_Project3_jpbutler0/src/cosc460_project3_jpbutler0/Cosc460_Project3_jpbutler0.java
@@ -0,0 +1,16 @@
+package cosc460_project3_jpbutler0;
+
+/**
+ * @author Jonathan Butler
+ */
+public class Cosc460_Project3_jpbutler0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }    
+}
