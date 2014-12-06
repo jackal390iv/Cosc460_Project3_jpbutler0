d6210f080fcef41fadf89601b6db7b818b62ef75
jackal390iv <jonpbiv@gmail.com>
2014-12-05 20:29:08 -0500

    finished

--- Cosc460_Project3_jpbutler0/src/cosc460_project3_jpbutler0/PageTable.java
+++ Cosc460_Project3_jpbutler0/src/cosc460_project3_jpbutler0/PageTable.java
@@ -7,16 +7,18 @@ package cosc460_project3_jpbutler0;
 public class PageTable {
 
     public PageTable(double val) {
-        System.out.println("The address " + val + " contains: ");
+        System.out.println("The address " + (int) val + " contains: ");
         try {
-            
-            
-            System.out.println("page number = " + " and offset = " + "\n");
+            double num = val / 4096;
+            double off = val % 4096;
+            System.out.println("page number = " + (int) num + " and offset = " + (int) off + "\n");
         } catch (Exception ex) {
+            System.out.println("\n" + "ERROR");
             System.out.println("Type: " + ex.getClass().getName());
+            System.out.println("Location: " + this.getClass().getName() + "." + Thread.currentThread().getStackTrace()[1].getMethodName());
             System.out.println("Cause: " + ex.getCause());
             System.out.println("Message: " + ex.getMessage());
-            System.out.println("Local Message: " + ex.getLocalizedMessage());
+            System.out.println("Local Message: " + ex.getLocalizedMessage() + "\n");
             //ex.printStackTrace();*/
         }
     }
--- Cosc460_Project3_jpbutler0/src/cosc460_project3_jpbutler0/Test.java
+++ Cosc460_Project3_jpbutler0/src/cosc460_project3_jpbutler0/Test.java
@@ -4,21 +4,41 @@ import java.util.Scanner;
 
 /**
  *
- * @author Reaper
+ * @author Jonathan Butler
  */
 public class Test {
-    private Scanner scan = new Scanner(System.in);
-    
+
+    private Scanner scan;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      new Test();
-    } 
-    public Test(){
-        do{
-            System.out.println("Please enter an adress: ");
-            new PageTable(scan.nextDouble());
-        }while(true);
+        new Test();
+    }
+
+    public Test() {
+        do {
+            try {
+                scan = new Scanner(System.in);
+                double val;
+                System.out.println("Please enter an adress: ");
+                String tmp = scan.nextLine();
+                if (tmp.contains("^")) {
+                    val = Math.pow(Double.parseDouble(tmp.substring(0, tmp.indexOf("^"))), Double.parseDouble(tmp.substring(tmp.indexOf("^") + 1)));
+                } else {
+                    val = Double.parseDouble(tmp);
+                }
+                new PageTable(val);
+            } catch (Exception ex) {
+                System.out.println("\n" + "ERROR");
+                System.out.println("Type: " + ex.getClass().getName());
+                System.out.println("Location: " + this.getClass().getName() + "." + Thread.currentThread().getStackTrace()[1].getMethodName());
+                System.out.println("Cause: " + ex.getCause());
+                System.out.println("Message: " + ex.getMessage());
+                System.out.println("Local Message: " + ex.getLocalizedMessage() + "\n");
+                //ex.printStackTrace();*/
+            }
+        } while (true);
     }
 }
